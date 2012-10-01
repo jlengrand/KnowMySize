fa8d0755e00bdb2f660ab396afc810964c664ba2
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-10-01 22:08:00 +0200

    Gets ListView elements displayed on screen
    Elements are static for now.

--- src/fr/lengrand/knowmysize/MainActivity.java
+++ src/fr/lengrand/knowmysize/MainActivity.java
@@ -2,15 +2,30 @@ package fr.lengrand.knowmysize;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	   // This is the Adapter being used to display the list's data
+    SimpleCursorAdapter mAdapter;
+
+    // These are the Contacts rows that we will retrieve
+    static final String[] friends = new String[] {"Paul", "Bob", "Jacques"};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter =
+        	      new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
+        ListView list = (ListView) findViewById(R.id.friendList);
+        list.setAdapter(adapter);
+        
     }
 
     @Override
