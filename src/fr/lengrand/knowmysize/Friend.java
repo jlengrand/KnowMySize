63fa1719854de3deb6f5d24a0eff17db7d5e262f
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-11-05 15:58:36 +0100

    Adds two root layers of datamodel

---
+++ src/fr/lengrand/knowmysize/Friend.java
@@ -0,0 +1,25 @@
+package fr.lengrand.knowmysize;
+
+import java.util.ArrayList;
+
+/**
+ * DescriBes an entity the user created. 
+ * A friend represent a unique element of the list 
+ * generated in MainActivity
+ * 
+ * Root component of our data model
+ * @author jll
+ *
+ */
+public class Friend {
+
+	private static int unknown = 0;
+	private static int man = 1;
+	private static int woman = 2;
+	
+	
+	private String name; 
+	private ArrayList<Clothe> clothes;
+	private int sexe = 0;
+	private int age;
+}
